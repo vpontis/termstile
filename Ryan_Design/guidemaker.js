94687f1b7d0cf467499862807f336465eff57903
Akash Badshah <akashbad4123@gmail.com>
2012-01-16 23:25:08 -0500

    added study guide maker

---
+++ Ryan_Design/guidemaker.js
@@ -0,0 +1,27 @@
+function createGuide() {
+	var guideArea = document.getElementById('guideArea');
+	var manyTerm = document.getElementById('manyTerm');
+	var input = manyTerm.value;
+	var terms = input.split(/\n|,\s/gm);
+	var summaries = new Array(terms.length);
+	var left = terms.length;
+	$.each(terms, function(index, value){
+		getText(terms[index],function(summary){
+			summaries[index]=summary[0]+" "+summary[1]+" "+summary[2];
+			console.log(summaries[index]);
+			if(--left==0){
+				makeDoc(terms, summaries);
+			}
+		});
+	});
+}
+
+function makeDoc(terms, summaries){
+	var guideText = "";
+	for(var i=0; i<terms.length; i++){
+		guideText += "<strong>"+terms[i]+"</strong><br />"+summaries[i]+"<br />";
+	}
+	var guideBox = document.getElementById('guideBox');
+	$(guideBox).html = guideText;
+	$(guideBox).css('display','block');
+}
