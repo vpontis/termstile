966b576fbea0e972add24c3634132edf821303ff
Ryan Lau <Ryan@Ryan-Laus-MacBook-Pro.local>
2012-01-16 22:15:08 -0500

    added login and signup boxes

--- Ryan_Design/tabswitch.js
+++ Ryan_Design/tabswitch.js
@@ -20,19 +20,45 @@ $(document).ready(function() {
 		$(".multiplesearch").hide();
 	});
 	// Login JS
-	$(".signin").click(function(e) {
+	$("#login").click(function(e) {
 		e.preventDefault();
-		$("fieldset#signin_menu").toggle();
-		$(".signin").toggleClass("menu-open");
+		$("fieldset.signin_menu").toggle();
+		$("#login").toggleClass("menu-open");
 	});
 
-	$("fieldset#signin_menu").mouseup(function() {
+	$("fieldset.signin_menu").mouseup(function() {
+		return false
+	});
+	$("fieldset.signup_menu").mouseup(function() {
 		return false
 	});
 	$(document).mouseup(function(e) {
 		if($(e.target).parent("a.signin").length == 0) {
 			$(".signin").removeClass("menu-open");
-			$("fieldset#signin_menu").hide();
+			$("fieldset.signin_menu").hide();
 		}
+	});
+	$(document).mouseup(function(e) {
+		if($(e.target).parent("a.signin").length == 0) {
+			$(".signin").removeClass("menu-open");
+			$("fieldset.signup_menu").hide();
+		}
+	});
+	$("#signup").click(function(e) {
+		e.preventDefault();
+		$("fieldset.signin_menu").toggle();
+		$("#signup").toggleClass("menu-open");
+	});
+	$("#login").click(function() {
+		$(".signup_menu").hide();
+	});
+	$("#login").click(function() {
+		$(".signin_menu").show();
+	});
+	$("#signup").click(function() {
+		$(".signup_menu").show();
+	});
+	$("#signup").click(function() {
+		$(".signin_menu").hide();
 	});
 });
