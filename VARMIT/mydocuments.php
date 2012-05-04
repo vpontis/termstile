b0ea7925de93313c5bd9e59832a287a65f10f497
Victor Pontis <vpontis@gmail.com>
2012-05-04 14:44:28 -0400

    added google analytics to VARMIT/VARMIT my docs file

--- VARMIT/mydocuments.php
+++ VARMIT/mydocuments.php
@@ -33,6 +33,19 @@
 				header('Location:index.php');
 			}
 		?>
+		<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-31428487-1']);
+		  _gaq.push(['_trackPageview']);
+		
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		
+		</script>
 	</head>
 	<body onLoad="initializePage('<?php echo $_COOKIE['email']?>')">
 		<!--<div class="bigthing">-->
